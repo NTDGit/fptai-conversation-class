a932cd9e56dfe904bcb9191708e7f9dfc42360eb
NGUYEN TAN DAT <60657191+NTDGit@users.noreply.github.com>
2021-03-10 13:15:08 +0700

    Add files via upload

---
+++ example.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+use ntdgit\FPTAI\FPT_Chatbot;
+$chatbot = new FPT_Chatbot(TRUE,'123456');
+
+$chatbot->sendText('Nội dung');
+$chatbot->sendImage('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png','XINCHAO');
+$chatbot->sendImage('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'); //WEB
+$chatbot->createQuickReply("Nội dung hiển thị trước khi có Quick Reply", 
+ array(
+    $chatbot->createQuickReplyButton('Lựa chọn 1','BlockName1',array("set_attributes"=>"value")),
+    $chatbot->createQuickReplyButton('Lựa chọn 2','BlockName2',array("date_birthday"=>"10/03/2021","sex"=>"male")),
+    $chatbot->createQuickReplyButton('Lựa chọn 3','BlockName1'),
+ )
+ );
+
+ $chatbot->sendTextCard("GoodDay",
+ array(
+     $chatbot->createButtonToBlock("TextButton1","BlockName1",array("tên_attributes"=>"giá trị")), 
+     $chatbot->createButtonToBlock("TextButton2","BlockName2")
+     )
+    ); 
+    $chatbot->sendCarousel(array(
+      $chatbot->creatCarousel("image_url","subtitle","title", $chatbot->createButtonToBlock("TextButton2","BlockName2"),"item_url"),
+      $chatbot->creatCarousel("<image url>","B1 Desc","New Carousel", $chatbot->createButtonToBlock("TextButton2","BlockName2"))
+    )
+    );
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'ntdgit/php-fptai-conversation-api',
+  ),
+  'versions' => 
+  array (
+    'ntdgit/php-fptai-conversation-api' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
